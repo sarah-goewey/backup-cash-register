445ff33a1eb740516ed3feacd63c6ce832206985
Sarah Goewey <sarah.goewey@yahoo.com>
2023-07-19 13:00:53 -0400

    first framework building

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
-const App = ()=> {
-  const { foo } = useSelector(state => state);
+const App = () => {
   return (
     <div>
-      <h1>Dealers Choice { foo }</h1>
+      <h1>Backup Cash Register</h1>
+      <h2>
+        <i>For when your point-of-sale is being a POS.</i>
+      </h2>
+      <hr />
+      <h3>Items:</h3>
+      <button>Calculate</button>
     </div>
   );
 };
